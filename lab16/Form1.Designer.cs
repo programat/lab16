ce923dd4cc7463a17de1f97b75b4736d433d3ecf
egord <egordets@gmail.com>
2022-12-27 01:21:05 +0300

    First commit

---
+++ lab16/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Construction_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.zedGraphControl1.Location = new System.Drawing.Point(1, 0);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(598, 366);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.AutoSizeChanged += new System.EventHandler(this.button1_Click);
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 335);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ lab16/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Construction_16
+{
+    public partial class Form1 : Form
+    {
+        GraphPane graphPane1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Main();
+        }
+
+        void  Main()
+        {
+            graphPane1 = zedGraphControl1.GraphPane;
+            double a = 0;
+            double b = 0;
+            PointPairList pplr = new PointPairList();
+            LineItem liner = graphPane1.AddCurve("Lag curve", pplr, Color.Red, SymbolType.None);
+
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            double y;
+            for (double x = 1; x <= 3; x += 0.1)
+            {
+                y = a / x + Math.Sqrt(x*x - 1);
+                PointPair ppr = new PointPair(x, y);
+                pplr.Add(ppr);
+            }
+            zedGraphControl1.AxisChange();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphPane1 = zedGraphControl1.GraphPane;
+            graphPane1.CurveList.Clear();
+            double a = 0;
+            double b = 0;
+
+            PointPairList pplr = new PointPairList();
+            PointPairList pplmin = new PointPairList();
+            PointPairList pplmax = new PointPairList();
+
+            try {
+                a = Convert.ToDouble(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            double n_min = 0, n_max = 0, n = 0;
+            double f = 1;
+            double y = a / f + Math.Sqrt(f*f - 1); ;
+            double y_min = y;
+            double y_max = -y;
+
+            for (double x = 1; x <= 3; x += 0.1)
+            {
+                y = a / x + Math.Sqrt(x*x - 1);
+                PointPair ppr = new PointPair(x, y);
+                pplr.Add(ppr);
+
+                if (y <= y_min)
+                {
+                    y_min = y;
+                    n_min = x;
+                }
+                if (y >= y_max)
+                {
+                    y_max = y;
+                    n_max = x;
+                }
+                n++;
+
+            }
+
+            for(int i = 0; i < 2; i++)
+            {
+                pplmax.Add(n_max, y_max*i);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                pplmin.Add(n_min, y_min*i);
+            }
+
+
+            LineItem liner = graphPane1.AddCurve("Curve", pplr, Color.Black, SymbolType.None);
+            LineItem liner_min = graphPane1.AddCurve("Min", pplmin, Color.SteelBlue, SymbolType.None);
+            LineItem liner_max = graphPane1.AddCurve("MAx", pplmax, Color.DarkRed, SymbolType.None);
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
